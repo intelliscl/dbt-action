7668ede158ab4a271f5cb85811939738d8bdd6a7
IS-Josh <48971328+IS-Josh@users.noreply.github.com>
2021-12-04 16:11:32 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ then
 elif [ -n "${DBT_TOKEN}" ]
 then
  echo trying to use DBT_TOKEN/databricks
- sed -i "s/_token_/${DBT_TOKEN}/g" ./profiles.yml
+ sed -i "s/_token_/${DBT_TOKEN}/g" ../profiles.yml
 else
   echo no tokens or credentials supplied
 fi
